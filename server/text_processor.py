c0931418e08c91fb149c2bba5c428bb2e68027f5
studAdam <126314769+studAdam@users.noreply.github.com>
2025-05-01 18:41:48 +0800

    Update text_processor.py

--- server/text_processor.py
+++ server/text_processor.py
@@ -1,17 +1,9 @@
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import nltk
 import spacy.cli
 import spacy
 import re
 
 spacy.cli.download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
-
-nltk.download("punkt")
-nltk.download("stopwords")
-
-stop_words = set(stopwords.words("english"))
 
 def remove_stopwords(text):
     doc = nlp(text)
@@ -31,3 +23,11 @@ def lemmatize(text):
     doc = nlp(text)
     lemmas = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
     return " ".join(lemmas)
+
+# # testing
+# sample_text = "Apple Inc. was founded in 1976 by Steve Jobs in California."
+# print("Original:", sample_text)
+# print("\n1. Clean Symbols:", clean_symbols(sample_text))
+# print("\n2. Remove Stopwords:", remove_stopwords(sample_text))
+# print("\n3. Lemmatize:", lemmatize(sample_text))
+# print("\n4. Enrich with Entities:\n", enrich_text_with_entities(sample_text))
